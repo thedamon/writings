67dae1affe9bee18bd86828672ca44e394746f0e
Damon Muma <damuma@gmail.com>
2023-08-14 21:05:16 -0400

    move posts to fiction, add doodles

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
@@ -13,7 +12,6 @@ module.exports = function(eleventyConfig) {
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
 
   // Alias `layout: post` to `layout: layouts/post.njk`
---
+++ doodle.sh
@@ -0,0 +1,2 @@
+# cat doodles.txt doodles.txt > poodles.md
+cat doodles-frontmatter.txt "/Users/damon/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/ESBS/doodles.txt" > doodles.md
